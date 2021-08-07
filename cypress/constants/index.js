c7e95d3ca21a648170a26634d4d6625e2d60ad43
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-08-07 12:14:43 +0300

    add constants

---
+++ cypress/constants/index.js
@@ -0,0 +1,6 @@
+export const _URLS = {
+  TEST_AUTOMATION_UNIVERSITY: {
+    HOME: 'https://testautomationu.applitools.com',
+    LEARNING_PATHS: 'https://testautomationu.applitools.com/learningpaths.html',
+  },
+};
