40af630c650cf42cf6017c44d3b0e651b373df86
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-08-07 13:36:01 +0300

    move test code to beforeEach block

--- cypress/integration/test-automation-university-applitools/learning_paths.spec.js
+++ cypress/integration/test-automation-university-applitools/learning_paths.spec.js
@@ -2,21 +2,20 @@ import { _URLS } from '../../constants';
 
 describe('Test Automation University Applitools', () => {
   context('Learning paths endpoint', () => {
-    it('should log all accessibility issues if present on the page', () => {
+    beforeEach(() => {
       cy.visit(_URLS.TEST_AUTOMATION_UNIVERSITY.LEARNING_PATHS);
       cy.injectAxe();
+    });
+
+    it('should log all accessibility issues if present on the page', () => {
       cy.checkA11y();
     });
 
     it('should log all accessibility issues on a single DOM element if both are present', () => {
-      cy.visit(_URLS.TEST_AUTOMATION_UNIVERSITY.LEARNING_PATHS);
-      cy.injectAxe();
       cy.checkA11y('img');
     });
 
     it('should log only the critical accessibility issues if present on the page', () => {
-      cy.visit(_URLS.TEST_AUTOMATION_UNIVERSITY.LEARNING_PATHS);
-      cy.injectAxe();
       cy.checkA11y(null, {
         includedImpacts: ['critical'],
       });
