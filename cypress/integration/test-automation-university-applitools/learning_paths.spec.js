58fc7851468b005af184132c1412f5d40119f0b7
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-08-07 12:15:57 +0300

    add a spec

---
+++ cypress/integration/test-automation-university-applitools/learning_paths.spec.js
@@ -0,0 +1,11 @@
+import { _URLS } from '../../constants';
+
+describe('Test Automation University Applitools', () => {
+  context('Learning paths endpoint', () => {
+    it('should log all accessibility issues if present', () => {
+      cy.visit(_URLS.TEST_AUTOMATION_UNIVERSITY.LEARNING_PATHS);
+      cy.injectAxe();
+      cy.checkA11y();
+    });
+  });
+});
