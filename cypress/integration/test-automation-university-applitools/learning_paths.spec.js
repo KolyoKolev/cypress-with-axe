333184a55e4843edbf915bb00306dc34941b03b4
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-08-07 13:18:26 +0300

    update tests titles

--- cypress/integration/test-automation-university-applitools/learning_paths.spec.js
+++ cypress/integration/test-automation-university-applitools/learning_paths.spec.js
@@ -2,13 +2,13 @@ import { _URLS } from '../../constants';
 
 describe('Test Automation University Applitools', () => {
   context('Learning paths endpoint', () => {
-    it('should log all accessibility issues if present', () => {
+    it('should log all accessibility issues if present on the page', () => {
       cy.visit(_URLS.TEST_AUTOMATION_UNIVERSITY.LEARNING_PATHS);
       cy.injectAxe();
       cy.checkA11y();
     });
 
-    it('should log all accessibility issues on a single DOM element if the formar are present', () => {
+    it('should log all accessibility issues on a single DOM element if both are present', () => {
       cy.visit(_URLS.TEST_AUTOMATION_UNIVERSITY.LEARNING_PATHS);
       cy.injectAxe();
       cy.checkA11y('img');
